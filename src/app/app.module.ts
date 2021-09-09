9e5634fdb7efcaa07adeb68ca677382c840e4d9f
Suraj <surajkumar.kumar835@gmail.com>
2021-09-09 16:47:13 +0530

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BiodataComponent } from './biodata/biodata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BiodataComponent
   ],
   imports: [
     BrowserModule,
